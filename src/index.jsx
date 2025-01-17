3d2389a26b757721e975bd9339f5091aed35ca78
carsonrb11 <155111205+carsonrb11@users.noreply.github.com>
2025-01-16 18:31:15 -0700

    Create index.jsx
    created index.jsx
    included Main Component
    made sure to import index.scss

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import { createRoot } from 'react-dom/clinet';
+
+//import statement to indicate that you need to bundle `./index.scss`
+import ".index.scss";
+
+//Main Compnent (will eventually use all others)
+const MovieMadnessApplication = () => {
+    return (
+        <div className="movie-madness">
+            <div>Good Morning!</div>
+        </div>
+    );
+};
+
+//Finds the root of your app
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+//Tells React to render your app in the root DOM element
+root.render(<MovieMadnessApplication />);
