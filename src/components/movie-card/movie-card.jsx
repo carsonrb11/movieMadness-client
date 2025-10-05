3933fa2fee811c9496d1e7db64843bfc7419076a
carsonrb11 <155111205+carsonrb11@users.noreply.github.com>
2025-10-04 22:21:44 -0600

    Update movie-card.jsx
    adding prop types

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div
@@ -9,3 +11,23 @@ export const MovieCard = ({ movie, onMovieClick }) => {
         </div>
     );
 };
+
+//Where all props constraints for the MovieCard are defined
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string,
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+            Birth: PropTypes.string.isRequired,
+            Death: PropTypes.string
+        }),
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }),
+        Featured: PropTypes.boolean.isRequired
+    })
+}
