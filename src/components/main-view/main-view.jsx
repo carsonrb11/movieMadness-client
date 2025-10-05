85a477d1da241c9ddc865a7a701e8d0cd02803ef
carsonrb11 <155111205+carsonrb11@users.noreply.github.com>
2025-10-04 22:21:26 -0600

    Update main-view.jsx
    Adding hooks

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,35 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Talladega Knight: The Ballad of Ricky Bobby",
-            image: "https://m.media-amazon.com/images/I/91yzZRen9rL._AC_UL480_FMwebp_QL65_.jpg",
-            description: "Description for Talladega Knights",
-            genre: "Comedy",
-            director: "Adam McKay"
-        },
-        {
-            id: 2,
-            title: "Shrek",
-            image: "https://m.media-amazon.com/images/I/81WLefsqEML._AC_UY327_FMwebp_QL65_.jpg",
-            description: "Description for Shrek",
-            genre: "Comedy",
-            director: "Andrew Adamson",
-        },
-        {
-            id: 3,
-            title: "Remember the Titans",
-            image: "https://m.media-amazon.com/images/I/51XXB53YV8L._AC_UY327_FMwebp_QL65_.jpg",
-            description: "Description for Remember the Titans",
-            genre: "Sports Drama",
-            director: "Boaz Yakin",
-        },
-    ]);
+    const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+    useEffect(() => {
+        fetch("")
+        .then((response) => response.json())
+        .then((data) => {
+            const moviesFromApi = data.docs.map((doc) => {
+                return {
+                    id: doc.key,
+                    title: doc.title,
+                    image: "",
+                    author: doc.author_name?.[0],
+                };
+            });
+            setMovies(moviesFromApi);
+        });
+    }, []);
 
     if (selectedMovie) {
         return (
