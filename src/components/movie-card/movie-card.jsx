8d649b52efd8752a3e06915e488026e09f2d7950
carsonrb11 <155111205+carsonrb11@users.noreply.github.com>
2026-01-14 22:47:03 -0700

    Add Link import to MovieCard component
    Imported Link from react-router-dom in movie-card.jsx to enable navigation functionality within the MovieCard component.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
